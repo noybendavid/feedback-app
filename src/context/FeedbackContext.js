8438b413f93df70ceda2e9d334221dbb1729bf08
noybendavid <noybd95@gmail.com>
2022-04-18 16:07:24 +0300

    complete json-server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,52 +1,56 @@
-import { createContext, useState } from 'react'
-import {v4 as uuidv4} from 'uuid'
+import { createContext, useState, useEffect } from 'react'
 
 const FeedbackContext = createContext()
 //for these [The components in app.js] to get access to our state were going to wrap
 //everything in a provider [same as we wrap everything with Router to use the Route component]
 
 export const FeedbackProvider = ({children}) => {
-
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: 'This item is feedback item 1',
-      rating: 10,
-    },
-    {
-      id: 2,
-      text: 'This item is feedback item 2',
-      rating: 9,
-    },
-    {
-      id: 3,
-      text: 'This item is feedback item 3',
-      rating: 7,
-    },
-  ])
-
+  const [isLoading, setIsLoading] = useState(true)
+  const [feedback, setFeedback] = useState([])
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false
   })
 
+  useEffect(() => {
+    fetchFeedback()
+  }, [])
+
+  //Fetch feedback
+  const fetchFeedback = async () => 
+  {
+    //We used proxy for http://localhost:5000 in package.json
+    const respone = await fetch(`/feedback?_sort=id&_order=desc`)
+    const data = await respone.json()
+    setFeedback(data)
+    setIsLoading(false)
+  }
+
   //Before we move it to context it was in the app.js->{
   //we want this here because this app.js its the main app component
   // and is where we have our feedback}
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if(window.confirm('Are you sure you want to delete ?')){
+      await fetch(`/feedback/${id}`, {method: 'DELETE'})
       //the feedback filter is going to return an array minus the one were deleting 
       // [that will not pass the test ; item.id===id will delete]
       setFeedback(feedback.filter((item) => item.id !== id ))
     }
   }
 
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch('/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newFeedback)
+    })
+    const data = await response.json()
     //state is immutable we cante just push new element
     //we need to create a copy of the array [...feedback]
     //and add the new - [newFeedback, ...feedback]
-    setFeedback([newFeedback, ...feedback])
+    setFeedback([data, ...feedback])
   }
 
   //Set item to be updated
@@ -59,8 +63,18 @@ export const FeedbackProvider = ({children}) => {
   }
 
   //Update Feedback item
-  const updateFeedback = (id, updItem) => {
-    setFeedback(feedback.map((item) => (item.id === id ? {...item, ...updItem} : item)
+  const updateFeedback = async (id, updItem) => {
+    const respone = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updItem)
+    })
+    //we update on the server - we gwt the respone back which is the update item
+    const data = await respone.json()
+
+    setFeedback(feedback.map((item) => (item.id === id ? {...item, ...data} : item)
     ))
   }
 
@@ -72,6 +86,7 @@ export const FeedbackProvider = ({children}) => {
   value={{
     feedback,
     feedbackEdit,
+    isLoading,
     deleteFeedback,
     addFeedback,
     editFeedback,
